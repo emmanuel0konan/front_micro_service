f2784b970e8cc1d5bbd1962d2f0e23e5bab5e173
emmanuel0konan <emmanuel.konan@epitech.eu>
2025-03-13 09:45:46 +0000

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  // Enable CORS with custom options
+  app.enableCors({
+    origin: ['https://front-end-react-tau.vercel.app'], // Allowed origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+    credentials: true, // Allow credentials (e.g., cookies)
+    allowedHeaders: 'Content-Type, Accept', // Allowed headers
+});
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
